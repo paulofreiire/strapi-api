293853e7982893ce9dff320cc2e8affe5d38c495
Paulo Freire <paulo.freiire@hotmail.com>
2021-02-11 21:54:45 -0300

    xD

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a1257d3471836e36599b4a35f5497ef1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9f5d987-f16b-4314-8d15-8893bdbbf44f'
+};
